2db2b8c956f72c0f3396226ec091010a794e798c
WhiteHorse101 <rishabh.saudagar2001@gmail.com>
2021-09-06 20:44:43 +0530

    All Files Added

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1>Global Air Tracking with Azure Maps</h1>
+
+Hey hi there folks 👋 <br>
+I made made this small project with the help of Microsoft Learn. In this project I have made use of Azure Maps for plotting the AQI [Air Quality Index] on the map. 
+This project was build using Python, Flask and an API called "World Air Quality Index" WAQI. 
+
+You can find more about this project <a href="https://docs.microsoft.com/en-gb/learn/modules/azure-maps-track-air-pollution/7-visualize-pollution-data">here</a>
+
+<br>
+
+
+<h2>Tech Stack</h2>
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) 
+![Flask](https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white)
+![Azure](https://img.shields.io/badge/azure-%230072C6.svg?style=for-the-badge&logo=azure-devops&logoColor=white)
+
+
+<h2> How to use this application </h2>
+
+You will have to fork this repo for using it. Once forked clone it and run the home.html file contained in templates folder. 
+Once you run the file the webpage will ask persmission to access your location if you give the application permission to access you location then your current location will be picked and the data of AQI will be shown. If your not willing to give your location then the default location of Microsoft's headquater in Redmond, Washington, United States will be used and the data will be shown. 
+
+The data once plotted on the map may look like this. 
+<img src="https://docs.microsoft.com/en-us/learn/advocates/azure-maps-track-air-pollution/media/final-output.png"></img>
+
---
+++ app.py
@@ -0,0 +1,65 @@
+import os, json
+from flask import Flask, render_template, request
+import requests
+
+# Load the Azure Maps key from the .env file.
+MAP_KEY = os.environ["MAP_KEY"]
+
+# Load the World Air Quality Index key from the .env file.
+WAQI_API_KEY = os.environ["WAQI_API_KEY"]
+WAQI_API_URL = https://api.waqi.info/map/bounds/?latlng={},{},{},{}&token={}
+
+# Initialize the Flask app.
+app = Flask(__name__)
+
+# Handle requests to the root of the website, returning the home page.
+@app.route("/")
+def home():
+    # Create data for the home page to pass the Maps key.
+    data = {"map_key": MAP_KEY}
+    # Return the rendered HTML page
+    return render_template("home.html", data=data)
+
+def get_color(aqi):
+    # Convert the AQI value to a color.
+    if aqi <= 50: return "#009966"
+    if aqi <= 100: return "#ffde33"
+    if aqi <= 150: return "#ff9933"
+    if aqi <= 200: return "#cc0033"
+    if aqi <= 300: return "#660099"
+    return "#7e0023"
+
+def load_aqi_data(lon1, lat1, lon2, lat2):
+    # Load the air quality data.
+    url = WAQI_API_URL.format(lat1, lon1, lat2, lon2, WAQI_API_KEY)
+    aqi_data = requests.get(url)
+
+    # Create a GeoJSON feature collection from the data.
+    feature_collection = {
+        "type" : "FeatureCollection",
+        "features" : []
+    }
+
+    for value in aqi_data.json()["data"]:
+        # Filter out empty values.
+        if value["aqi"] != "-":
+            feature_collection["features"].append({
+                "type" : "Feature",
+                "properties" : {
+                    "color" : get_color(int(value["aqi"]))
+                },
+                "geometry" : {
+                    "type":"Point",
+                    "coordinates":[value['lon'], value['lat']]
+                }
+                })
+
+    return feature_collection
+
+@app.route("/aqi", methods = ["GET"])
+def get_aqi():
+    # Get the bounds from the request.
+    bounds = request.args["bounds"].split(",")
+
+    # Load the AQI data and create the GeoJSON for the specified bounds.
+    return json.dumps(load_aqi_data(bounds[0], bounds[1], bounds[2], bounds[3]))
